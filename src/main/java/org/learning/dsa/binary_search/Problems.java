c26c676e20a0fac8d8ee809d54952e2dd8a026e0
Piyush Raj <piyushrajkhg@gmail.com>
2025-02-07 01:13:49 +0530

    added code for first and last occurance problem

--- src/main/java/org/learning/dsa/binary_search/Problems.java
+++ src/main/java/org/learning/dsa/binary_search/Problems.java
@@ -44,4 +44,43 @@ public class Problems {
             return result;
         }
     }
+
+    private static class FirstAndLastOccurrence {
+        public static int[] optimal(int[] nums, int x) {
+            int[] result = new int[] {-1,-1};
+            int n = nums.length;
+            int low = 0, high = n - 1;
+
+            // first
+            while (low <= high) {
+                int mid = low + (high-low)/2;
+                int curr = nums[mid];
+                if (curr >= x) {
+                    if (curr == x) result[0] = mid;
+                    high = mid - 1;
+                } else {
+                    low = mid+1;
+                }
+            }
+
+            low = 0;
+            high = n - 1;
+
+            // last
+            while (low <= high) {
+                int mid = low + (high-low)/2;
+                int curr = nums[mid];
+                if (curr == x) {
+                    result[1] = mid;
+                    low = mid+1;
+                } else if (curr < x) {
+                    low = mid+1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+            return result;
+        }
+    }
 }
