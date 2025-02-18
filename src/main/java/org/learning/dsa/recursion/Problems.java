a1ea2377a8852cf4fa2b6be9524b32efaff592d0
Piyush Raj <piyushrajkhg@gmail.com>
2025-02-18 23:20:56 +0530

    added solution for combination sum and subset sum pattern

--- src/main/java/org/learning/dsa/recursion/Problems.java
+++ src/main/java/org/learning/dsa/recursion/Problems.java
@@ -1,7 +1,11 @@
 package org.learning.dsa.recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Problems {
     public static void main(String[] args) {
@@ -14,6 +18,10 @@ public class Problems {
 
         System.out.println(ExistsSubsequenceWithSumK.checkSubsequenceSum(new int[]{1, 2, 3, 4, 5}, 8));
         System.out.println(ExistsSubsequenceWithSumK.checkSubsequenceSum(new int[]{4, 3, 9, 2}, 10));
+
+        System.out.println(CombinationSum.combinationSum(new int[]{2, 3, 5, 4}, 7));
+
+        System.out.println(CombinationSum2.combinationSum2(new int[] {2, 1, 2, 7, 6, 1, 5}, 8));
     }
 
     static class Pow {
@@ -116,6 +124,109 @@ public class Problems {
             if (idx == n) return 0;
 
             return helper(idx+1, n, k - nums[idx], nums) + helper(idx+1, n, k, nums);
+        }
+    }
+
+    // https://takeuforward.org/plus/dsa/recursion/faqs-medium/combination-sum
+    static class CombinationSum {
+        public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+            List<List<Integer>> answer = new ArrayList<>();
+            helper(0, candidates.length, target, new ArrayList<>(), answer,candidates);
+            return answer;
+        }
+
+        private static void helper(int idx, int n, int target, List<Integer> tmp, List<List<Integer>> answer, int[] candidates) {
+            if (target == 0) {
+                answer.add(new ArrayList<>(tmp));
+                return;
+            }
+            if (target < 0) return;
+            if (idx == n) return;
+
+
+            // pick
+            tmp.add(candidates[idx]);
+            helper(idx, n, target - candidates[idx], tmp, answer, candidates);
+            tmp.removeLast();
+
+            // non pick
+            helper(idx+1, n, target, tmp, answer, candidates);
+        }
+    }
+
+    // https://takeuforward.org/plus/dsa/recursion/faqs-medium/combination-sum-ii
+    static class CombinationSum2 {
+        public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            Arrays.sort(candidates);
+            Set<List<Integer>> answer = new HashSet<>();
+
+            helper(0, candidates.length, target, new ArrayList<>(), answer, candidates);
+            return answer.stream().toList();
+        }
+
+        private static void helper(int idx, int length, int target, List<Integer> tmp, Set<List<Integer>> answer, int[] candidates) {
+            if (target == 0) {
+                answer.add(new ArrayList<>(tmp));
+                return;
+            }
+
+            if (target < 0) return;
+            if (idx == length) return;
+
+            // pick
+            tmp.add(candidates[idx]);
+            helper(idx+1, length, target - candidates[idx], tmp, answer, candidates);
+            tmp.removeLast();
+
+            // non-pick
+            helper(idx+1, length, target, tmp, answer, candidates);
+        }
+
+
+    }
+
+    // https://takeuforward.org/plus/dsa/recursion/faqs-medium/subsets-i
+    static class Subset1 {
+        public static List<Integer> subsetSums(int[] nums) {
+            List<Integer> result = new ArrayList<>();
+            helper(0, 0, nums, result);
+            return result;
+        }
+
+        private static void helper(int idx, int sum, int[] nums, List<Integer> ans) {
+            if (idx == nums.length) {
+                ans.add(sum);
+                return;
+            }
+
+            // pick
+            helper(idx + 1, sum + nums[idx], nums, ans);
+
+            // non pick
+            helper(idx + 1, sum, nums, ans);
+        }
+    }
+
+    //
+    static class Subset2 {
+        public static List<List<Integer>> subsetsWithDup(int[] nums) {
+            Arrays.sort(nums);
+            Set<List<Integer>> ans = new TreeSet<>();
+            helper(0, nums.length, new ArrayList<>(), ans, nums);
+            return new ArrayList<>(ans);
+        }
+
+        private static void helper(int idx, int n, List<Integer> tmp, Set<List<Integer>> ans, int[] nums) {
+            if (idx == n) {
+                ans.add(new ArrayList<>(tmp));
+                return;
+            }
+
+            tmp.add(nums[idx]);
+            helper(idx+1, n, tmp, ans, nums);
+            tmp.removeLast();
+
+            helper(idx+1, n, tmp, ans, nums);
         }
     }
 }
